11b70293ad9a38aabb51b681586aca3dc485872f
Vpank <Garenemy@mail.ru>
2016-04-28 18:50:39 +0800

    Движения
    Добавил скрипт движения объекта

---
+++ Assets/script/move.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public Transform player;
+    public float speed;
+    public float jumping;
+    int i;
+    private Rigidbody2D juping;
+    // Use this for initialization
+    void Start () {
+        
+        player.position = new Vector3(0, 0, 0);
+        juping = gameObject.GetComponent<Rigidbody2D>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+            float h = Input.GetAxis("Horizontal");
+            juping.AddForce((Vector2.right * speed) * h);
+        
+        
+        if (Input.GetButtonDown("Jump")) { 
+
+            juping.AddForce(Vector2.up * jumping);
+        }
+    }
+}
