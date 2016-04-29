d4968c779739b6155959fe19bd37b593a848d7a3
MyFavoriteHell <proto4el@gmail.com>
2016-04-29 21:42:26 +0200

    движение
    изменена система движения, теперь он не скользит, но двигается в воздухе
    рывками

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -10,18 +10,24 @@ public class move : MonoBehaviour {
     private Rigidbody2D juping;
     // Use this for initialization
     void Start () {
-        
-      
-        juping = gameObject.GetComponent<Rigidbody2D>();
+
+        //transform.position = Vector3.zero;
+       // player.position = transform.position;
+       juping = gameObject.GetComponent<Rigidbody2D>();
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-       
-            float h = Input.GetAxis("Horizontal");
-            juping.AddForce((Vector2.right * speed) * h);
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.D)) {
+            player.position += new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.position += new Vector3(-speed, 0, 0);
+        }
+        
         
         
         if (Input.GetButtonDown("Jump")) { 
